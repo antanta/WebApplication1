d8faf07c0b2e481b4acb15aedc04dfbffa08f927
Angel Nikolov <angel.nikolov@bulpros.com>
2017-09-07 17:46:29 +0300

    Added TypeScript examples

--- WebApplication1/scripts/app.js
+++ WebApplication1/scripts/app.js
@@ -1,7 +1,67 @@
+/// <reference path ="../scripts/typings/jquery/jquery.d.ts"/> 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function sayHello() {
     var compiler = document.getElementById("compiler").value;
     var framework = document.getElementById("framework").value;
-    framework = '3232 angel dassdds';
     return "Hello from " + compiler + " and " + framework + "!";
 }
+function printLabel(labelledObj) {
+    console.log(labelledObj.label);
+}
+var Animal = (function () {
+    function Animal() {
+    }
+    Animal.prototype.move = function (distance) {
+        console.log(this.name + " moved by " + distance);
+    };
+    return Animal;
+}());
+var Cat = (function () {
+    function Cat(name, weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    ;
+    Cat.prototype.reset = function (int) {
+        debugger;
+        console.log('cat\'s reset');
+    };
+    ;
+    Cat.prototype.move = function (distance) {
+        if (distance === void 0) { distance = 5; }
+        console.log(this.name + " ran " + distance);
+    };
+    ;
+    return Cat;
+}());
+var SiameseCat = (function (_super) {
+    __extends(SiameseCat, _super);
+    function SiameseCat(name, weight) {
+        var _this = _super.call(this, name, weight) || this;
+        _this.name = name;
+        _this.weight = weight;
+        return _this;
+    }
+    ;
+    SiameseCat.prototype.reset = function (int) {
+        console.log('this is siamese run');
+        _super.prototype.reset.call(this, int);
+    };
+    return SiameseCat;
+}(Cat));
+$(document).ready(function () {
+    var c1 = new Cat("ordinary cat", 5);
+    var c2 = new SiameseCat("siamese cat", 10);
+    c1.move();
+    c2.reset(2);
+});
 //# sourceMappingURL=app.js.map
--- WebApplication1/scripts/app.ts
+++ WebApplication1/scripts/app.ts
@@ -1,10 +1,61 @@
-﻿function sayHello() {
+﻿/// <reference path ="../scripts/typings/jquery/jquery.d.ts"/> 
 
-
-
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+function sayHello() {
+    let compiler = (document.getElementById("compiler") as HTMLInputElement).value;
     let framework = (document.getElementById("framework") as HTMLInputElement).value;
-
-    framework = '3232 angel dassdds';
     return `Hello from ${compiler} and ${framework}!`;
 }
+
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+abstract class Animal {
+    name: string;
+    weight: number;
+    abstract reset(int: number): void;
+    move(distance: number): void {
+        console.log(`${this.name} moved by ${distance}`);
+    }
+}
+
+class Cat implements Animal {
+    constructor(name: string, weight: number) {
+        this.name = name;
+        this.weight = weight;
+    };
+    reset(int: number): void {
+        debugger;
+        console.log('cat\'s reset');
+    };
+    name: string;
+    weight: number;
+    move(distance: number = 5): void {
+        console.log(`${this.name} ran ${distance}`);
+    };
+}
+
+class SiameseCat extends Cat {
+    constructor(name: string, weight: number) {
+        super(name, weight);
+        this.name = name;
+        this.weight = weight;
+    };
+    reset(int: number): void {
+        console.log('this is siamese run');
+        super.reset(int);
+    }
+}
+
+$(document).ready(function () {
+    let c1 = new Cat("ordinary cat", 5);
+    let c2 = new SiameseCat("siamese cat", 10);
+
+    c1.move();
+    c2.reset(2);
+
+});
