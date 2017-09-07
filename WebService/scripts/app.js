f5fe86c7dd1f3ca01638ab1e6ef23554e8b6232b
Angel Nikolov <angel.nikolov@bulpros.com>
2017-09-07 14:33:20 +0300

    Refining project structure

--- WebService/scripts/app.js
+++ WebService/scripts/app.js
@@ -1,6 +1,7 @@
 function sayHello() {
     var compiler = document.getElementById("compiler").value;
     var framework = document.getElementById("framework").value;
+    framework = '3232 angel dassdds';
     return "Hello from " + compiler + " and " + framework + "!";
 }
 //# sourceMappingURL=app.js.map
--- WebService/scripts/app.ts
+++ WebService/scripts/app.ts
@@ -1,5 +1,10 @@
 ﻿function sayHello() {
+
+
+
     const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+    let framework = (document.getElementById("framework") as HTMLInputElement).value;
+
+    framework = '3232 angel dassdds';
     return `Hello from ${compiler} and ${framework}!`;
 }
